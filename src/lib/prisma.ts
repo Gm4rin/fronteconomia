88ce88f95c8d901f3ddb21a68050a604d9846db8
Gustavo Marin <Gmarin2017@outlook.com>
2026-03-04 08:46:45 -0300

    fix: tentando resolver erro de build e tipagem

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,7 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// Não passamos nada dentro do new PrismaClient()
-// Ele vai buscar a variável DATABASE_URL sozinho!
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
